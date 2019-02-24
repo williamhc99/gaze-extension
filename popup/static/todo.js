a3733bbd1a7f86321db22cc9e1778405c9e5d9dc
williamhc99 <35355529+williamhc99@users.noreply.github.com>
2019-02-24 11:40:48 -0500

    add remove button

--- popup/static/todo.js
+++ popup/static/todo.js
@@ -97,6 +97,28 @@ function autocomplete(input, arr) {
 
 autocomplete(document.getElementById("goal"), countries);
 
+
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 7; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
 /* Handles entering value */
 // get the key pressed
 document.body.onkeydown = function(key) {
@@ -111,5 +133,18 @@ document.body.onkeydown = function(key) {
     items.appendChild(li);
 
     document.getElementById("goal").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
   }
 };
