ca98a8d5a9190cbd74d2a252e09f2062512d2e66
williamhc99 <wh6chen@uwaterloo.ca>
2019-03-20 20:22:17 -0400

    final product with labels from 50 pics

--- scrapper.py
+++ scrapper.py
@@ -3,11 +3,14 @@ import requests
 import cv2
 import wget
 import os
+import io
 from bs4 import BeautifulSoup, SoupStrainer
 from io import BytesIO
 from PIL import Image
 from google.cloud import storage
 from tempfile import TemporaryFile
+from google.cloud import vision
+from google.cloud.vision import types
 
 client = storage.Client()
 bucket = client.get_bucket('gaze_pictures')
@@ -15,9 +18,11 @@ basewidth = 2048
 http = httplib2.Http()
 status, response = http.request('https://apod.nasa.gov/apod/archivepix.html')
 counter = 0
+goodLabels = open('imagelabels.txt').read().splitlines()
+client = vision.ImageAnnotatorClient()
 for imageLink in BeautifulSoup(response, "lxml", parse_only=SoupStrainer('a')):
 # for i in range(9):
-    if counter > 10:
+    if counter >= 50:
         break
     if hasattr(imageLink, 'href') and str(imageLink['href']).startswith('ap'):
         # imagePage , content = http.request('https://apod.nasa.gov/apod/ap19020' + str(i+1) + '.html')
@@ -43,24 +48,49 @@ for imageLink in BeautifulSoup(response, "lxml", parse_only=SoupStrainer('a')):
                         hsize = int(float(height)*float(wpercent))
                         img_resized = img.resize((basewidth,hsize), Image.ANTIALIAS)
                         img_resized.save(filename)
-                        print('resized!')
+                        # print('resized!')
                     
                     if height > 1080 and width > 1920:
                         img = Image.open(filename)
                         height = img.size[1]
                         width = img.size[0]
-                        print('HEIGHT: ',height)
-                        print('WIDTH: ',width)
+                        # print('HEIGHT: ',height)
+                        # print('WIDTH: ',width)
                     
 
-    
+                    file_name = os.path.join(
+                        os.path.dirname(__file__),
+                        filename)
 
-                    # blob = bucket.blob('pic'+str(counter)+'.jpg')
-                    # blob.upload_from_filename(filename)
-                    # os.remove(filename)
+                    with io.open(file_name, 'rb') as image_file:
+                        content = image_file.read()
 
+                    image = types.Image(content=content)
+                    response = client.label_detection(image=image)
+                    labels = response.label_annotations
+
+                
+                    # print('Labels:')
+                    score = 0
+                    for label in labels:
+                        if (label.description in goodLabels):
+                            score+=1
+                        else: 
+                            goodLabels.append(label.description)
+                            print(label.description, file=open("imagelabels.txt", "a"))
+                    
+                    percentage = score/len(labels)
+                    print(percentage)
+
+                    if (percentage > 0.6){
+                        blob = bucket.blob('pic'+str(counter)+'.jpg')
+                        blob.upload_from_filename(filename)
+                    }
+                    os.remove(filename)
 
                     counter += 1
             except Exception as e:
                 print(e)
                 print('##some weird shit going on w this file')
+
+print(goodLabels)
--- tester.py
+++ tester.py
@@ -1,20 +1,47 @@
-import cv2
-from urllib.request import urlopen
+# import cv2
+# from urllib.request import urlopen
 
-imageLinks = open("imageurl.txt")
-counter = 0
-for url in imageLinks:
-  if counter > 10:
-    break
+# imageLinks = open("imageurl.txt")
+# counter = 0
+# for url in imageLinks:
+#   if counter > 10:
+#     break
 
-  with urlopen(url) as img:
-    with open(str(counter)+'pic.jpg', "w+") as file:
-      cv2.imwrite(file,img)
+#   with urlopen(url) as img:
+#     with open(str(counter)+'pic.jpg', "w+") as file:
+#       cv2.imwrite(file,img)
 
-  print(url) 
-  # wget.download(url)
+#   print(url) 
+#   # wget.download(url)
 
-  # filename = str(link['href'])[11:]
-  # print(filename)
-  counter += 1
+#   # filename = str(link['href'])[11:]
+#   # print(filename)
+#   counter += 1
 
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'M82Magnet_SOFIA_2412.jpg')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    print(label.description)
